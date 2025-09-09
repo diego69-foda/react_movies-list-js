1d442720d25e6200f9c28e1e4987d0c669414776
diego69-foda <diego.bkuhn@gmail.com>
2025-09-09 10:49:20 -0300

    uma pequena modificação (soy burro pacas)

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -5,7 +5,7 @@ import movies from '../../api/movies.json';
 export const MovieList = () => (
   <div className="movie-list">
     {movies.map(movie => (
-      <MovieCard key={movie.id} movie={movie} />
+      <MovieCard movie={movie} />
     ))}
   </div>
 );
